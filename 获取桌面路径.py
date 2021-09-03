9c917558a5b0e37b26db432b3a58a90bafa46d05
1161678627 <1161678627@qq.com>
2021-09-03 17:21:34 +0800

    获取桌面路径

---
+++ 获取桌面路径.py
@@ -0,0 +1,9 @@
+﻿import winreg
+
+#获取桌面路径
+def get_desktop():
+    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER,r'Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders')
+    return winreg.QueryValueEx(key, "Desktop")[0]
+
+if __name__ == '__main__':
+    print(get_desktop())
